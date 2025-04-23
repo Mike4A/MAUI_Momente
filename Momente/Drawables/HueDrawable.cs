9c83975d1f86c0a9344aea32c9477ec762269096
Michael Fink <michael.fink@live.at>
2025-04-23 19:14:33 +0200

    Speeded up the drawing process by increasing steps by 3

--- Momente/Drawables/HueDrawable.cs
+++ Momente/Drawables/HueDrawable.cs
@@ -19,8 +19,9 @@ namespace Momente.Drawables
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            canvas.StrokeSize = 1;
-            for (int x = -12; x < dirtyRect.Width + 12; x++)
+            int strokeSize = 3;
+            canvas.StrokeSize = strokeSize;
+            for (int x = -12; x < dirtyRect.Width + 12; x += strokeSize)
             {
                 float hue = (x + 1) / dirtyRect.Width;
                 canvas.StrokeColor = Color.FromHsla(hue, Saturation, Luminosity);
